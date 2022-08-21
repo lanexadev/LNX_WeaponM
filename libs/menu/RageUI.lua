017489f9984f2a7e267ba7b0f14b9f1da7b2ad2b
Z2SK <mcgaming707@gmail.com>
2022-08-22 00:25:36 +0200

    .

--- libs/menu/RageUI.lua
+++ libs/menu/RageUI.lua
@@ -12,7 +12,7 @@
 --- @see RageUI
 ---
 
-print("^4RageUI - https://github.com/iTexZoz/RageUI - OpenSource Advanced UI Api^0")
+--print("^4RageUI - https://github.com/iTexZoz/RageUI - OpenSource Advanced UI Api^0")
 
 function math.round(num, numDecimalPlaces)
     return tonumber(string.format("%." .. (numDecimalPlaces or 0) .. "f", num))
